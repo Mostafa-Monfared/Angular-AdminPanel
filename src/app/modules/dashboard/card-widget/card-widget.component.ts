9d9742489348c924a37d9aebc7305b1982d96c54
mohammaddehqani <dehgani88@gmail.com>
2024-01-27 16:23:19 +0330

    edit card in dashboard

--- src/app/modules/dashboard/card-widget/card-widget.component.ts
+++ src/app/modules/dashboard/card-widget/card-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-widget',
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardWidgetComponent implements OnInit {
 
-  constructor() { }
+  previousWeekSales: number;
+  currentWeekSales: number;
+  previousWeekIncome : number;
+  currentWeekIncome : number;
+  previousWeekVisitors : number;
+  currentWeekVisitors : number;
+  currentWeekOrders : number;
+  previousWeekOrders : number;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.previousWeekSales = 2000;
+    this.currentWeekSales = 7000;
+    this.previousWeekIncome = 6000;
+    this.currentWeekIncome = 3100;
+    this.previousWeekOrders = 2000;
+    this.currentWeekOrders = 1600;
+    this.previousWeekVisitors = 4054100;
+    this.currentWeekVisitors = 8000;
+  }
+
+  calculateSalesDifference(): number {
+    return this.currentWeekSales - this.previousWeekSales;
+  }
+
+  PercentageDifferenceSales(): number {
+    const difference = this.calculateSalesDifference();
+    var percentage = (difference / this.previousWeekSales) * 100;
+    return parseFloat(percentage .toFixed(2));
+  }
+
+
+  calculateIncomeDifference(): number {
+    return this.currentWeekIncome - this.previousWeekIncome;
+  }
+
+  PercentageDifferenceIncome(): number {
+    const difference = this.calculateIncomeDifference();
+    var percentage =  (difference / this.previousWeekIncome) * 100;
+    return parseFloat(percentage .toFixed(2));
+  }
+
+  calculateOrdersDifference(): number {
+    return this.currentWeekOrders - this.previousWeekOrders;
+  }
+
+  PercentageDifferenceOrders(): number {
+    const difference = this.calculateOrdersDifference();
+    var percentage =  (difference / this.previousWeekOrders) * 100;
+    return parseFloat(percentage .toFixed(2));
+  }
+
+  calculateVisitorsDifference(): number {
+    return this.currentWeekVisitors - this.previousWeekVisitors;
+  }
+
+  PercentageDifferenceVisitors(): number {
+    const difference = this.calculateVisitorsDifference();
+    var percentage  =  (difference / this.previousWeekVisitors) * 100;
+    return parseFloat(percentage .toFixed(2));
+  }
+
+  ngOnInit(): any {
+
   }
 
 }
