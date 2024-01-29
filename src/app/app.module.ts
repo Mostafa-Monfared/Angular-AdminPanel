e972768f6adc5a314b9e6cfdda99d127868217cb
Mostafa Esmaeili <mostafa.es9919@gmail.com>
2024-01-29 16:08:23 +0330

    conflict were replaced by incoming changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { NgApexchartsModule } from 'ng-apexcharts';
-@NgModule({
-  declarations: [
-    AppComponent,
-     ],
-=======
 import { SharedModule } from './shared/shared.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
-import {EventsComponent} from './modules/dashboard/events/events.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventsComponent  ],
->>>>>>> 5186438 (Add apexchart)
+    AppComponent
+      ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-<<<<<<< HEAD
-=======
     SharedModule,
     DashboardModule,
     NgApexchartsModule,
     
 
->>>>>>> 5186438 (Add apexchart)
   ],
   bootstrap: [AppComponent]
 })
--- src/app/modules/dashboard/events/events.component.ts
+++ src/app/modules/dashboard/events/events.component.ts
@@ -1,26 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartComponent } from "ng-apexcharts";
-<<<<<<< HEAD
-import {
-  ApexNonAxisChartSeries,
-  ApexResponsive,
-  ApexChart,
-  ApexAxisChartSeries,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
-  ApexLegend,
-  ApexStroke,
-  ApexXAxis,
-  ApexFill,
-  ApexTooltip
-=======
 
 import {
   ApexNonAxisChartSeries,
   ApexResponsive,
   ApexChart
->>>>>>> 5186438 (Add apexchart)
 } from "ng-apexcharts";
 
 export type ChartOptions = {
@@ -37,42 +21,11 @@ export type ChartOptions = {
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-<<<<<<< HEAD
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
-
-
-  constructor() {
-        
-    this.chartOptions = {
-      series: [44, 55, 13, 43, 22],
-      chart: {
-        type: "donut"
-      },
-      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              position: "bottom"
-            }
-          }
-        }
-      ]
-    };
-
-   }
-=======
   
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
 
   constructor() {
->>>>>>> 5186438 (Add apexchart)
 
    }
 
