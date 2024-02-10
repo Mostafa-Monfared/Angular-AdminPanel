d191b75060d58fe7fff596cfcf05ccbbb7e51f21
Mostafa Esmaeili <mostafa.es9919@gmail.com>
2024-02-10 10:04:15 +0330

    add

--- src/app/modules/users/users/users.component.ts
+++ src/app/modules/users/users/users.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent {
 
+  constructor(private router: Router) { }
+
+  onAddUser(event:any) {
+    this.router.navigateByUrl('layout/users/add');
+};
 }
